ef864b901487ad80147c3c0bda341a0ef9875dfe
Abanoub Harby <abanoubharby33@gmail.com>
2022-03-21 23:15:45 +0200

    cors working

--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder => {
+                          builder.WithOrigins("https://abanoubhm.github.io");
+                      });
+});
 builder.Services.AddDbContext<AngularFinalDemoContext>(options => options.UseSqlServer("Data Source=tcp:productsdbdbserver.database.windows.net,1433;Initial Catalog=ProductsDb_db;User Id=handmade@productsdbdbserver;Password=H@ndmade"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,7 +26,7 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
