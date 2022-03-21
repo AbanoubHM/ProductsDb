7d99a092d58769cce2fb53878d739190c5cf523d
Abanoub Harby <abanoubharby33@gmail.com>
2022-03-21 19:36:13 +0200

    Cors

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using ProductsDb.Models;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
+builder.Configuration.AddUserSecrets<Program>();
+var connectionString = builder.Configuration.GetConnectionString("DbConnection");
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options => {
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder => {
+                          builder.WithOrigins("http://localhost:4200",
+                              "http://localhost:4321");
+                      });
+});
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AngularFinalDemoContext>(options => options.UseSqlServer("Data Source=tcp:productsdbdbserver.database.windows.net,1433;Initial Catalog=ProductsDb_db;User Id=handmade@productsdbdbserver;Password=H@ndmade"));
+builder.Services.AddDbContext<AngularFinalDemoContext>(options => options.UseSqlServer(connectionString));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,7 +33,7 @@ if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
